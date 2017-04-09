c0e29d06ec86327de523f16b5c827f20efce9809
kh <kathryn.hartog@gmail.com>
2017-04-08 21:12:21 -0400

    display data in a table, sort by column and filter with input box

--- js/dndRef.js
+++ js/dndRef.js
@@ -1,31 +1,29 @@
-angular.module('dndRef', []).controller('weaponsCtrl', function($scope) {
+angular.module('dndRef', []).controller('weaponsCtrl', function($scope, $http) {
   
   $scope.weapons = [];
   
-  getJSON('data/weapons.json',
-    function(err, data) {
-      if (err != null) {
-        console.log('Error: ' + err);
-      } else {
-        console.log(data[0].name);
-      }
+  $http.get("data/weapons.json")
+    .then(function (response) {
+      let weapons = response.data;
+      
+      angular.forEach(weapons, function(value, key) {
+        if (typeof value.properties !== 'undefined') {
+          let properties_str = value.properties.join(', ');
+          weapons[key].properties_str = properties_str;
+        }
+      });
+      
+      console.log(weapons);
+      
+      $scope.weapons = weapons;
     });
+  
+  $scope.sortProperty = 'name';
+  $scope.reverse = true;
 
+  $scope.sortBy = function(propertyName) {
+    $scope.reverse = ($scope.sortProperty === propertyName) ? !$scope.reverse : false;
+    $scope.sortProperty = propertyName;
+  };
     
 });
-
-
-var getJSON = function(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.onload = function() {
-    var status = xhr.status;
-    if (status == 200) {
-      callback(null, xhr.response);
-    } else {
-      callback(status);
-    }
-  };
-  xhr.send();
-};

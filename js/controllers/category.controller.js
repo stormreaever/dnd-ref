248472182e9c995575b3b84b3d676a469c5ac421
kh <kathryn.hartog@gmail.com>
2017-05-13 13:26:00 -0400

    add support for deep-links. TODO: add links to all category views

--- js/controllers/category.controller.js
+++ js/controllers/category.controller.js
@@ -1,6 +1,9 @@
 app.controller('categoryCtrl', function(saveData, $scope, $routeParams, $http, $filter) {
   
   $scope.category = $routeParams.category;
+  $scope.item = $routeParams.item;
+  
+  console.log($scope.item);
   
   $scope.searchText = '';
   
@@ -14,39 +17,6 @@ app.controller('categoryCtrl', function(saveData, $scope, $routeParams, $http, $
     "detailsUrl": ""
   }
   
-  var saved_data = saveData.get();
-  
-  if (saved_data.length > 0) {
-    if (saved_data[0].data_type != $scope.category || 
-        saved_data[saved_data.length - 1].data_type != $scope.category) {
-      getData('data/' + $scope.category + '.json');
-    } else {
-      $scope.items = saved_data;
-    }
-  } else {
-    getData('data/' + $scope.category + '.json');
-  }
-  
-  function getData(url) {
-    $http.get(url).then(function (response) {
-        data = response.data;
-        saveData.set(data);
-        $scope.items = data;
-      });
-  };
-  
-  
-  $scope.sortBy = function(propertyName) {
-    $scope.reverse = ($scope.sortProperty === propertyName) ? !$scope.reverse : false;
-    $scope.sortProperty = propertyName;
-  };
-  
-  $scope.selectItem = function(selectedItem) {
-    //$scope.selectedItem = selectedItem;
-    $scope.searchText = selectedItem;
-    $scope.searchChange();
-  };
-  
   $scope.searchChange = function() {
     let searchList = $filter('filter')($scope.items, {name: $scope.searchText}, true);
     
@@ -58,9 +28,51 @@ app.controller('categoryCtrl', function(saveData, $scope, $routeParams, $http, $
     }
   };
   
+  $scope.selectItem = function(selectedItem) {
+    //$scope.selectedItem = selectedItem;
+    $scope.searchText = selectedItem;
+    $scope.searchChange();
+  };
+  
+  var saved_data = saveData.get();
+  
+  if (saved_data.length > 0) {
+    if (saved_data[0].data_type != $scope.category || 
+        saved_data[saved_data.length - 1].data_type != $scope.category) {
+      getData('data/' + $scope.category + '.json');
+    } else {
+      $scope.items = saved_data;
+      if ($scope.item !== undefined) {
+        $scope.selectItem($scope.item);
+      }
+    }
+  } else {
+    getData('data/' + $scope.category + '.json');
+  }
+  
+  
+  
+  $scope.sortBy = function(propertyName) {
+    $scope.reverse = ($scope.sortProperty === propertyName) ? !$scope.reverse : false;
+    $scope.sortProperty = propertyName;
+  };
+  
+  
+  
   $scope.clearSearch = function() {
     $scope.searchText = '';
     $scope.searchChange();
+  };
+  
+  function getData(url) {
+    $http.get(url).then(function (response) {
+        data = response.data;
+        saveData.set(data);
+        $scope.items = data;
+        if ($scope.item !== undefined) {
+          $scope.selectItem($scope.item);
+        }
+      });
   };
   
 });
